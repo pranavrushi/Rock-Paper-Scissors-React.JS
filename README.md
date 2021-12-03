fb82caaf7eeed4891aa3f0ab020b7a27d688fe77
pranavrushi <90118186+pranavrushi@users.noreply.github.com>
2021-12-03 13:25:47 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+# Build Rock, Paper and Scissors Game with React JS
+
+This repository contains code for rock,paper and scissors game in reactjs.
+
+
+## Building and running on localhost
+
+First install dependencies:
+
+```sh
+npm install
+```
+
+To run in hot module reloading mode:
+
+```sh
+npm start
+```
+
+To create a production build:
+
+```sh
+npm run build-prod
+```
+
+## Runningg
+
+Open the file `dist/index.html` in your browser
+
+
+
---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import Modal from "./Modal";
+const Footer = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => {
+    setModal(!modal);
+  };
+
+  return (
+    <>
+      <footer className="footer">
+        <div class="attribution">
+          Coded by :: Pranav Rushi T.
+        </div>
+        <button className="rules" onClick={toggle}>
+          Rules
+        </button>
+      </footer>
+      {modal ? <Modal toggle={toggle} /> : null}
+    </>
+  );
+};
+
+export default Footer;
